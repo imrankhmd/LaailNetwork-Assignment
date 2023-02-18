5d68e05788b4faec5e19f4c17ccb4c6421aae4dc
Imran Khan Mohammad <92031287+imrankhmd@users.noreply.github.com>
2023-02-18 20:29:17 +0530

    Delete models directory

--- models/User.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const userSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    mobileNumber: { type: String, required: true },
-
-})
-const UserModel = mongoose.model("user", userSchema)
-module.exports = UserModel;
-
